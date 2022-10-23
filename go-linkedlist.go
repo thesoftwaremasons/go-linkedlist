c0657f9845696e49bbfeca01ab3f592e87d0e683
Babafemi Ogundipe <smiles2k6@gmail.com>
2022-10-23 12:19:16 +0100

    feat: first commit

---
+++ go-linkedlist.go
@@ -0,0 +1,267 @@
+package go_linkedlist
+
+import "fmt"
+
+type node struct {
+	data int
+	next *node
+}
+type doubleNode struct {
+	prev *doubleNode
+	data int
+	next *doubleNode
+}
+
+func AddDoubleNode(list *doubleNode, val int) *doubleNode {
+	//2,5,4
+	head := list
+	if list.next == nil {
+		newNode := &doubleNode{
+			prev: head,
+			data: val,
+			next: nil,
+		}
+		list.next = newNode
+	} else {
+		for list.next != nil {
+			list = list.next
+		}
+		newNode := &doubleNode{
+			prev: list,
+			data: val,
+			next: nil,
+		}
+		list.next = newNode
+	}
+	return head
+}
+
+func AddNode(list *node, val int) *node {
+	//1,5,7,10,49
+	newNode := &node{
+		data: val,
+		next: nil,
+	}
+	if list.next == nil {
+
+		list.next = newNode
+	} else {
+		temp := list
+		for temp.next != nil {
+			temp = temp.next
+		}
+		temp.next = newNode
+	}
+	return list
+}
+func DeleteCircularNode(list *doubleNode, pos int) *doubleNode {
+	head := list
+	count := 1
+	if pos == 1 {
+		head = list.next
+
+	} else {
+		for list.next != nil {
+
+			if count == pos-1 {
+				list.next = list.next.next
+				break
+			}
+			list = list.next
+			count++
+		}
+	}
+	return head
+}
+
+func ReverseCicularNode(list *doubleNode) *doubleNode {
+	var prev *doubleNode = nil
+	var middle *doubleNode = nil
+	next := list
+	for next.next != nil {
+		prev = middle
+		middle = next
+		next = next.next
+		middle.next = prev
+	}
+	return middle
+}
+func DisplayDoubleLinkedList(list *doubleNode) {
+	for list.next != nil {
+		fmt.Println(list.data)
+		list = list.next
+	}
+	fmt.Println(list.data)
+}
+
+func DeleteBeginingNode(list *node) *node {
+	list = list.next
+	return list
+
+}
+func DeleteFromEndOfNode(list *node) *node {
+	holder := list
+	for list.next != nil {
+		if list.next.next == nil {
+			list.next = nil
+			break
+		}
+		list = list.next
+	}
+	return holder
+}
+func DeleteFromPosition(list *node, position int) *node {
+	holder := list
+	count := 1
+	for list.next != nil {
+		if count == position-1 {
+			list.next = list.next.next
+			break
+		}
+		list = list.next
+		count++
+	}
+	return holder
+}
+
+func ReverseArray(list *node) *node {
+	prev := &node{}
+	middle := &node{}
+	next := list
+	for next.next != nil {
+		prev = middle
+		middle = next
+		next = middle.next
+		middle.next = prev
+	}
+	return middle
+
+}
+
+func AddCircularLinkedList(list *node, val int) *node {
+	//2.5,6,2,8
+
+	head, temp := list, list
+	//temp := list
+	newNode := &node{}
+	newNode.data = val
+
+	if temp == temp.next {
+		newNode.next = temp
+		temp.next = newNode
+	} else {
+		for head != temp.next {
+			temp = temp.next
+		}
+
+		temp.next = newNode
+		newNode.next = head
+
+	}
+
+	return head
+}
+func AddInfrontCircularLinkedList(list *node, val int) *node {
+	head := list
+
+	temp := &node{
+		data: val,
+		next: head,
+	}
+	for head != list.next {
+		list = list.next
+	}
+	list.next = temp
+	list = temp
+
+	return list
+}
+
+func AddAtTheEndOfCircularList(list *node, val int) *node {
+	head := list
+	newNode := &node{
+		data: val,
+		next: nil,
+	}
+	for head != list.next {
+		list = list.next
+	}
+	newNode.next = list.next
+	list.next = newNode
+	return head
+}
+
+func DeleteAtTheTopOfCircularList(list *node) *node {
+	head := list
+	for head != list.next {
+		list = list.next
+	}
+	head = head.next
+	list.next = head
+	return head
+}
+
+func DeleteOfCircularListByPosition(list *node, position int) *node {
+
+	head := list
+	count := 1
+	for head != list.next {
+		if count == position {
+			list = list.next
+			head.next = list
+			break
+		}
+		list = list.next
+		count++
+	}
+	if head == list.next {
+		list = list.next
+		list.next = head
+	}
+	return head
+}
+
+func DisplayCircularLinkedList(list *node) {
+	head := list
+	for head != list.next {
+		fmt.Println(list.data)
+		list = list.next
+	}
+	fmt.Println(list.data)
+}
+
+func Display(list *node) {
+	temp := list
+	for temp != nil {
+		fmt.Println(temp.data)
+		temp = temp.next
+	}
+}
+func MergeLinkedList(firstNode *node, secondNode *node) *node {
+	holder, head := &node{}, &node{}
+	if firstNode.data < secondNode.data {
+		holder = firstNode
+		firstNode = firstNode.next
+	} else {
+		holder = secondNode
+		secondNode = secondNode.next
+	}
+	head = holder
+	for firstNode != nil && secondNode != nil {
+		if firstNode.data < secondNode.data {
+			holder.next = firstNode
+			holder = firstNode
+			firstNode = firstNode.next
+		} else {
+			holder.next = secondNode
+			holder = secondNode
+			secondNode = secondNode.next
+		}
+	}
+	if firstNode != nil {
+		holder.next = firstNode
+	} else {
+		holder.next = secondNode
+	}
+	return head
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thesoftwaremasons/go-linkedlist
+
+go 1.19
